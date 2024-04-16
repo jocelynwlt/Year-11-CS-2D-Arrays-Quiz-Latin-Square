20b8e2bf41a599b36bff5bf01533c4d91d099407
jocelyn <jocelynwlt2026@student.cis.edu>
2024-04-16 12:39:56 +0800

    finish

--- src/LatinSquare.java
+++ src/LatinSquare.java
@@ -8,6 +8,9 @@ public class LatinSquare {
      */
     public static int targetCheckSum1(int rowSize) {
         int sum=0;
+        for(int i=0;i<=rowSize;i++){
+            sum+=i;
+        }
         return sum;
     }
 
@@ -19,6 +22,9 @@ public class LatinSquare {
      */
     public static int targetCheckSum2(int rowSize) {
         int prod=1;
+        for(int i=1;i<=rowSize;i++){
+            prod*=i;
+        }
         return prod;
     }
 
@@ -32,7 +38,20 @@ public class LatinSquare {
      *         AND that every row's product in array2d is equal to checkSum2
      */
     public static boolean isLatinRows(int[][] array2d, int checkSum1, int checkSum2) {
-        return false;
+
+        for (int[] row : array2d) {
+            int sum = 0;
+            int product = 1;
+            for (int i : row) {
+                sum += i;
+                product *= i;
+            }
+            if (sum != checkSum1 || product != checkSum2) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
     /**
@@ -44,7 +63,24 @@ public class LatinSquare {
      *         AND that every column's product in array2d is equal to checkSum2
      */
     public static boolean isLatinColumns(int[][] array2d, int checkSum1, int checkSum2) {
-        return false;
+
+        for (int i=0;i<array2d.length;i++) {
+            int sum = 0;
+            int product = 1;
+            for(int a=0;a< array2d[i].length;a++){
+                sum+=array2d[a][i];
+                product*=array2d[a][i];
+            }
+
+            if(sum!=checkSum1||product!=checkSum2){
+                return false;
+            }
+
+        }
+
+        return true;
+
+
     }
 
 
@@ -55,6 +91,23 @@ public class LatinSquare {
      *         Hint: make use of isLatinRows and isLatinColumns
      */
     public static boolean isLatinSquare(int[][] array2d) {
+        boolean tf=false;
+        for (int i=0;i<array2d.length;i++) {
+            int sum = 0;
+            int product = 1;
+            int colsum = 0;
+            int colproduct = 1;
+            for (int a = 0; a < array2d[i].length; a++) {
+                sum += array2d[i][a];
+                product *= array2d[i][a];
+                colsum += array2d[a][i];
+                colproduct *= array2d[a][i];
+            }
+            if(isLatinRows(array2d, sum, product)==false||isLatinColumns(array2d,colsum,colproduct)==false){
+                return false;
+            }
+            return true;
+        }
         return false;
     }
 
